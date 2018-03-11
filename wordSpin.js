ed590f2dec6fa7f81a30d60e733dbdd714d1c93f
kwin555 <ktn555@att.net>
2018-03-11 12:00:08 -0500

    refractored code

--- wordSpin.js
+++ wordSpin.js
@@ -1,13 +1,8 @@
 function spinWords(string){
   'use strict';
-  let wordsArray = string.split(' ');
-  let lettersArray = wordsArray.map( word => word.split(''));
-  for (let i = 0; i < lettersArray.length; i += 1) {
-    if (lettersArray[i].length >= 5) {
-      lettersArray[i] = lettersArray[i].reverse();
-    } 
-  }
- wordsArray = lettersArray.map(word =>  word.join(''));
-  return wordsArray.join(' ')
+  let wordsArray = string.split(' ').map(function (word) {
+    return (word.length >= 5) ? word.split('').reverse().join('') : word
+  });
+  return wordsArray.join(' ');
 }
-console.log(spinWord("This is another test")); 
+console.log(spinWords("This is another test")); 
